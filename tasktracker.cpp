9dd190d51f133db108168391ec8a6f8847d35a12
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2025-04-11 19:35:09 -0400

    Add hollow CLI structure

---
+++ tasktracker.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+class CLI {
+    public:
+    int id;
+    std::string description;
+    std::string status;
+    std::string createdAt;
+    std::string updatedAt;
+};
+
