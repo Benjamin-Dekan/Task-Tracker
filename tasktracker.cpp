31031bb0e8a5af0f4b75f672dff8441603e6a64f
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2025-04-11 21:06:50 -0400

    add function: in progress

--- tasktracker.cpp
+++ tasktracker.cpp
@@ -16,8 +16,46 @@ int main () {
 
     while(1) {
         std::string userInput;
-        std::cin >> userInput;
-        
+        std::getline(std::cin, userInput);
+
+        // Checks if userInput is an "add", "update" or "delete" command
+        if (userInput[0] == 'a') {
+            std::string addInput;
+            int checkFormat = 0;
+            for (int i = 0; i < userInput.length(); i++) {
+                if (userInput[i] == '"') {
+                    int j = 0;
+                    int checkInfinite = 0;
+                    checkFormat = 1;
+                    addInput.clear();
+                    while (userInput[i + j] != '"' && (i + j) < userInput.length()) {
+                        addInput += userInput[i + j];
+                        j++;
+                        if (checkInfinite > 10000) {
+                            std::cout << "Add command used incorrectly. Please ensure proper quotations around the message you wish to add, i.e: add \"Buy Milk\"." << std::endl;
+                        }
+                    }
+
+                    if ((i + j) >= userInput.length() || userInput[i + j] !='"') {
+                        std::cout << "Add command used incorrectly. Missing closing quotation mark." << std::endl;
+                    }
+                    break;
+                }
+            }
+            if (checkFormat <= 0) {
+                std::cout << "Add command used incorrectly. Please ensure proper quotations around the message you wish to add, i.e: add \"Buy Milk\"."<< std::endl;
+            }
+
+
+
+
+        } else if(userInput[0] == 'u') {
+
+        } else if (userInput[0] == 'd') {
+
+        } else {
+            std::cout << "Unrecognized command. Please enter a valid command" << std::endl;
+        }
 
     }
 
